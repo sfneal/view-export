a3042bb4f388880bfcb4014c2581c2701bed84a8
Stephen Neal <stephen.neal14@gmail.com>
2021-02-04 15:42:07 -0500

    START working on PDF export tests

--- tests/PdfExportActionTest.php
+++ tests/PdfExportActionTest.php
@@ -2,11 +2,18 @@
 
 namespace Sfneal\ViewExport\Tests;
 
+use Sfneal\ViewExport\Pdf\PdfExportAction;
+
 class PdfExportActionTest extends TestCase
 {
-    /** @test */
+    /**
+     * @test
+     * @throws \Dompdf\Exception
+     */
     public function pdf_can_be_exported()
     {
+//        $path = (new PdfExportAction('tests', 'test'))->execute();
+
         $this->assertTrue(true);
     }
 }
