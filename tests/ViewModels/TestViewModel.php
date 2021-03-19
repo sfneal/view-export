f460438cf253045c08ccc2299d2584c9e040db0c
Stephen Neal <stephen.neal14@gmail.com>
2021-03-19 12:44:52 -0400

    ADD `__construct()` & `string()` methods to TestViewModel

--- tests/ViewModels/TestViewModel.php
+++ tests/ViewModels/TestViewModel.php
@@ -6,5 +6,23 @@ use Sfneal\ViewModels\AbstractViewModel;
 
 class TestViewModel extends AbstractViewModel
 {
-    public $view = 'test';
+    /**
+     * TestViewModel constructor.
+     *
+     * @param string $view
+     */
+    public function __construct(string $view = 'test')
+    {
+        $this->view = $view;
+    }
+
+    /**
+     * Return a string.
+     *
+     * @return string
+     */
+    public function string(): string
+    {
+        return "Here's a string!";
+    }
 }
