503c970f02fa4382b043f423c473404310e5a0da
Stephen Neal <stephen.neal14@gmail.com>
2021-06-10 10:09:26 -0400

    ADD `PdfStatus` interface for implementing `pdfExists()` & `pdfProcessing()` methods on Models with PDF files
    BUMP CHANGELOG.md.

---
+++ src/Pdf/Interfaces/PdfStatus.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Sfneal\ViewExport\Pdf\Interfaces;
+
+
+interface PdfStatus
+{
+    // todo: add tests
+
+    /**
+     * Determine if the Model's PDF exists.
+     *
+     * @return bool
+     */
+    public function pdfExists(): bool;
+
+    /**
+     * Determine if the Model's PDF is processing.
+     *
+     * @return bool
+     */
+    public function pdfProcessing(): bool;
+}
